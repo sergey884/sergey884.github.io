96a8fcdf9dde534d3cec251fd04cb8c89383d252
avanesyans <sergey.avanesyan@dowjones.com>
2023-04-15 10:35:12 -0400

    feat: [draft] Add new materials

--- src/materials/css.js
+++ src/materials/css.js
@@ -8,6 +8,32 @@ export const css = [
         url: "https://css-tricks.com/snippets/css/complete-guide-grid/",
         type: "article",
       },
+      {
+        title: "Test CSS GRID rules",
+        url: "https://grids.help/",
+        type: "apps",
+      },
+      {
+        title: "Learn CSS Grid - A 13 Minute Deep Dive",
+        url: "https://www.youtube.com/watch?v=EiNiSFIPIQE",
+        type: "youtube",
+      },
+    ],
+  },
+  {
+    id: "cssflexbox",
+    title: "CSS Flexbox",
+    links: [
+      {
+        title: "A Complete Guide to Flexbox",
+        url: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/",
+        type: "article",
+      },
+      {
+        title: "Test CSS FLEXBOX rules",
+        url: "https://flexbox.help/",
+        type: "apps",
+      },
     ],
   },
 ];
